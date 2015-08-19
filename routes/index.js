33aa079b48988fc4b5bc3e402b2ea3845105c1dd
Reme Le Hane <rlehane@tagworldiwde.com>
2015-08-19 17:02:31 +0200

    v0.4.1: clean logs

--- routes/index.js
+++ routes/index.js
@@ -53,9 +53,7 @@ router.route('/movie-details/:id')
 
 		// Does not persist on Heroku instance
 		if (aResults) {
-			console.log(aResults);
 			if(!aResults.local_img) {
-				console.log(aResults.local_img);
 				downloader.download('http://image.tmdb.org/t/p/w342' + movieData.poster_path, imgDir);
 				downloader.on('done', function (aResponse) {
 					moviesService.updateImg(aRequest.params.id, function (aError, aResults) {});
