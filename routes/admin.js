c5b1010d3b6190792bf40a86ccae2006cd51ba78
Reme Le Hane <reme.lehane@gmail.com>
2015-08-24 08:02:23 +0200

    v0.8.0: Notification prep

--- routes/admin.js
+++ routes/admin.js
@@ -6,12 +6,20 @@ var moviesService = require('../services/movies-service');
 var locale = require('../locale/en_gb');
 var downloader = require('downloader');
 var fs = require('fs-extra');
+var flash = require('connect-flash');
+var session = require('express-session');
 
 var movieData, location, limit = 10, pagination = [], messageSent,
 	imgDir = 'public/images/w342/',
 	thumbDir = 'public/images/w92/';
 
-storage.initSync();
+router.use(session({
+	secret: 'keyboard cowboy',
+	resave: false,
+	saveUninitialized: true,
+	cookie: { maxAge: 60000 }
+}))
+router.use(flash());
 
 router.route('/')
 .get(function (aRequest, aResponse) {
