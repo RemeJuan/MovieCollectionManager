c97e50d2bf79885ccc98e717c078165b0b4b49bf
Reme Le Hane <rlehane@tagworldiwde.com>
2015-08-24 18:25:24 +0200

    v0.8.0: Auto-hide notification and close button action.

--- public/javascripts/scripts.js
+++ public/javascripts/scripts.js
@@ -2,10 +2,20 @@ function toggleMenu () {
 	$('#menu').slideToggle(350);
 }
 
-function deleteItem (e) {
+function deleteItem () {
 	$('#modal-dialog').addClass('md-active');
+}
+
+function closeNotification() {
+	$('.md-toast').removeClass('toast-active');
 }
 
 $('#modal-cancel').on('click', function () {
 	$('#modal-dialog').removeClass('md-active');
+});
+
+$(document).ready(function() {
+	setTimeout(function() {
+		$('.md-toast').removeClass('toast-active');
+	}, 3000)
 });
